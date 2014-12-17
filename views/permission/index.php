c82ab2ed14fa8b75daa9ef649ec05bbcc81dac4a
Igor Chepurnoy <igorzfort@gmail.com>
2014-12-17 14:25:33 +0200

    Update index.php

--- views/permission/index.php
+++ views/permission/index.php
@@ -5,8 +5,8 @@ use yii\helpers\Html;
 use yii\widgets\Pjax;
 
 /**
- * @var yii\web\View                       $this
- * @var yii\data\ActiveDataProvider        $dataProvider
+ * @var yii\web\View $this
+ * @var yii\data\ActiveDataProvider $dataProvider
  * @var yii2mod\rbac\models\AuthItemSearch $searchModel
  */
 $this->title = 'Permission';
@@ -15,27 +15,25 @@ $this->render('/layouts/_sidebar');
 ?>
 <div class="role-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?php echo Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Permission', ['create'], ['class' => 'btn btn-success']) ?>
+        <?php echo Html::a('Create Permission', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php
-    Pjax::begin([
-        'enablePushState' => false,
-    ]);
-    echo GridView::widget([
+    <?php Pjax::begin(['enablePushState' => false]); ?>
+    
+    <?php echo GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
             'name',
             'description:ntext',
-            ['class' => 'yii\grid\ActionColumn',],
+            ['class' => 'yii\grid\ActionColumn'],
         ],
     ]);
-    Pjax::end();
     ?>
-
+    
+    <?php Pjax::end(); ?>
 </div>
