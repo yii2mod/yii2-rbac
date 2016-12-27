f66efc918fda6ded0ef8eecf0e2a61891ec7bd49
igor-chepurnoi <igorzfort@gmail.com>
2016-12-27 20:54:02 +0200

    added RbacAsset

---
+++ RbacRouteAsset.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace yii2mod\rbac;
+
+/**
+ * Class RbacRouteAsset
+ *
+ * @package yii2mod\rbac
+ */
+class RbacRouteAsset extends RbacAsset
+{
+    /**
+     * @var array
+     */
+    public $js = [
+        'js/rbac-route.js',
+    ];
+}
